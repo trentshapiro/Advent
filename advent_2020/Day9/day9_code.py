0a5a210cebecea71b45e448427ef463a2deb1dcd
trent shapiro <trentshapiro@gmail.com>
2020-12-09 00:02:26 -0600

    day 9

--- advent_2020/Day9/day9_code.py
+++ advent_2020/Day9/day9_code.py
@@ -8,17 +8,14 @@ data_in = [int(i.replace('\n','')) for i in data_in]
 #Part 1
 print('PART 1: ')
 repeat_value = 0
-for idx, value in enumerate(data_in):
-    if idx < 25:
-        continue
-    else:
-        prev = data_in[idx-25:idx]
-        current_value = value      
-        pairs = set([i+j for i in prev for j in prev if i!=j])
-        if current_value not in pairs:
-            print('value not in pairs found! ', current_value)
-            repeat_value = current_value
-            break
+for idx, value in enumerate(data_in[25:]):
+    prev = data_in[idx:idx+25]
+    current_value = value      
+    pairs = set([i+j for i in prev for j in prev if i!=j])
+    if current_value not in pairs:
+        print('value not in pairs found! ', current_value)
+        repeat_value = current_value
+        break
 
 
 #Part 2
@@ -26,15 +23,11 @@ print('\nPART 2: ')
 contiguous_set = None
 found_flag = False
 for set_length in range(2,30):
-    if not found_flag:
-        for idx, value in enumerate(data_in):
-            if idx < set_length:
-                continue
-            elif sum(data_in[idx-set_length:idx]) == repeat_value:
-                print('CONTIGUOUS SET FOUND OF LENGTH', set_length)
-                contiguous_set = data_in[idx-set_length:idx]
-                found_flag = True
-                print('Sum of min and max:', min(contiguous_set)+max(contiguous_set))
-                break
-    else:
-        break
+    for idx, value in enumerate(data_in[idx:]):
+        if sum(data_in[idx:idx+set_length]) == repeat_value:
+            print('CONTIGUOUS SET FOUND OF LENGTH', set_length)
+            contiguous_set = data_in[idx:idx+set_length]
+            found_flag = True
+            print('Sum of min and max:', min(contiguous_set)+max(contiguous_set))
+            break
+
