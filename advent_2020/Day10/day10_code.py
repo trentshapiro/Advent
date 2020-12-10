b324b5c0898a557a001ea39a81022e3bca4b11e2
trent shapiro <trentshapiro@gmail.com>
2020-12-10 01:12:25 -0600

    day 10

--- advent_2020/Day10/day10_code.py
+++ advent_2020/Day10/day10_code.py
@@ -18,45 +18,33 @@ print(diffs.count(1) * diffs.count(3))
 #part 2
 print('\nPART 2:')
 
-#convenient class
-class node:
-    def __init__(self, name, children):
-        self.name = name
-        self.children = children
-
-#create nodes
-nodes = []
+children_dict = {}
 for i in volt_list:
-    name = i
-    children = [j for j in volt_list if j-i in (1,2,3)]
-    nodes.append(node(i,children))
-
-#get a node object from its name
-def get_node(name):
-    return [i for i in nodes if i.name == name][0]
+    children_dict.update({
+        i:[j for j in volt_list if j-i in (1,2,3)]
+    })
 
 #store number values we already calculated
 numbers_searched = {}
 
 #find all roots
 def find_root(starting_node):
-    children = starting_node.children
+    children = children_dict[starting_node]
     final_value = 0
     
     #count when we hit the final node, otherwise get children
     if children == []:
         return 1
     else:
-        children_nodes = [get_node(i) for i in children]
-        for node in children_nodes:
+        for child in children:
             #if we've already calulated downstream, cool, otherwise calculate
-            if node.name in numbers_searched.keys():
-                this_value = numbers_searched[node.name]
+            if child in numbers_searched.keys():
+                child_value = numbers_searched[child]
             else:
-                this_value = find_root(node)
-                numbers_searched.update({node.name:this_value})
-            final_value += this_value
+                child_value = find_root(child)
+                numbers_searched.update({child:child_value})
+            final_value += child_value
     return final_value
 
-final_value = find_root(nodes[0])
+final_value = find_root(0)
 print(final_value)
