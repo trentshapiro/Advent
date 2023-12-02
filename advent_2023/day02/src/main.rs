e9f962c622e8a9e8b1c3cf26377f0f69b62edc99
imadethis-smile <trentshapiro@gmail.com>
2023-12-02 11:24:41 -0600

    solve in rust
    solve in rust

--- advent_2023/day02/src/main.rs
+++ advent_2023/day02/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use regex::Regex;
+//use regex::Regex;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -9,23 +9,91 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn get_game_number(game_line:&str) -> &str {
-    let first_split = game_line.split(":").collect().into_iter().nth(0);
-    let second_split = first_split.split(" ").unwrap().collect::<Vec<&str>>().last();
-    println!("{}",second_split);
+fn is_success(game_line:&str) -> bool {
+    let no_game = game_line.split(": ").collect::<Vec<&str>>().into_iter().nth(1).unwrap();
+    let all_commas = str::replace(no_game, ";",",");
+    let all_numbers = all_commas.split(", ").collect::<Vec<&str>>();
 
-    return second_split
+    for num in all_numbers {
+        let count = num.split(" ")
+                       .collect::<Vec<&str>>()
+                       .first()
+                       .unwrap()
+                       .to_string()
+                       .parse::<i32>()
+                       .unwrap();
+        let color = num.split(" ")
+                       .collect::<Vec<&str>>()
+                       .last()
+                       .unwrap()
+                       .to_string();
+        if color == "red" && count > 12 {
+            return false
+        }
+        if color == "green" && count > 13 {
+            return false
+        }
+        if color == "blue" && count > 14 {
+            return false
+        }
+    }
+    return true
 }
 
+fn calc_power(game_line:&str) -> i32 {
+    let no_game = game_line.split(": ").collect::<Vec<&str>>().into_iter().nth(1).unwrap();
+    let all_commas = str::replace(no_game, ";",",");
+    let all_numbers = all_commas.split(", ").collect::<Vec<&str>>();
+
+    let mut max_red = 0;
+    let mut max_green = 0;
+    let mut max_blue = 0;
+
+    for num in all_numbers {
+        let count = num.split(" ")
+                       .collect::<Vec<&str>>()
+                       .first()
+                       .unwrap()
+                       .to_string()
+                       .parse::<i32>()
+                       .unwrap();
+        let color = num.split(" ")
+                       .collect::<Vec<&str>>()
+                       .last()
+                       .unwrap()
+                       .to_string();
+
+        if color == "red" && count > max_red {
+            max_red = count;
+        }
+        if color == "green" && count > max_green {
+            max_green = count;
+        }
+        if color == "blue" && count > max_blue {
+            max_blue = count;
+        }
+    }
+
+    return max_red*max_blue*max_green
+}
+
+
 fn main() {
+    let mut total_success = 0;
+    let mut total_power = 0;
     // File must exist in the current path
     if let Ok(lines) = read_lines("./day02_input.txt") {
         // Consumes the iterator, returns an (Optional) String
-        for line in lines {
+        for (i, line) in lines.enumerate() {
             if let Ok(line_value) = line {
-                println!("{}", &line_value);
-                println!("{}", get_game_number(&line_value));
+                if is_success(&line_value) {
+                    total_success += i+1;
+                }
+                total_power += calc_power(&line_value);
             }
         }
     }
+    
+    println!("Total success: {}",total_success);
+    println!("Total power: {}",total_power);
 }
