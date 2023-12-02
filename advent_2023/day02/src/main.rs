4bba47a4836803eeafb3edac8acc5d6cf71a0fd4
root <root@Cyberpower-PC>
2023-12-02 09:59:06 -0600

    fail at rust, do in python, return to rust later

--- advent_2023/day02/src/main.rs
+++ advent_2023/day02/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use regex::Regex;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn get_game_number(game_line:&str) -> &str {
+    let first_split = game_line.split(":").collect().into_iter().nth(0);
+    let second_split = first_split.split(" ").unwrap().collect::<Vec<&str>>().last();
+    println!("{}",second_split);
+
+    return second_split
+}
+
 fn main() {
-    println!("Hello, world!");
+    // File must exist in the current path
+    if let Ok(lines) = read_lines("./day02_input.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(line_value) = line {
+                println!("{}", &line_value);
+                println!("{}", get_game_number(&line_value));
+            }
+        }
+    }
 }
