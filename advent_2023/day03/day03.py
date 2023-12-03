67e69b69c575b836a6d1d54a34f7ea9833ebfc3a
imadethis-smile <trentshapiro@gmail.com>
2023-12-03 14:53:25 -0600

    cleanup

--- advent_2023/day03/day03.py
+++ advent_2023/day03/day03.py
@@ -36,7 +36,7 @@ for row_number, row in enumerate(a):
         position_values = [a[x][y] for (x,y) in apply_mask(row_number, position)]
         check_chars = [i for i in position_values if not i.isnumeric() and i != "."]
         if len(check_chars) > 0:
-            number_value = int(row[position[0]:position[1]])
+            number_value = int(row[slice(*position)])
             valid_total += number_value
 
 print(valid_total)
@@ -49,17 +49,18 @@ for row_number, row in enumerate(a):
     if len(star_positions) == 0:
         continue
 
-    for position in star_positions:
+    for left_pos, right_pos in star_positions:
         touch_numbers = []
-        left_pos = position[0]-1
-        right_pos = position[1]+1
+        left_bound = left_pos - 1
+        right_bound = right_pos + 1
 
         for idx in range(0,3):
-            row_values = [i.span() for i in re.finditer("[0-9]{1,3}",a[row_number+idx-1])]
+            row_idx = row_number+idx-1
+            row_values = [i.span() for i in re.finditer("[0-9]{1,3}",a[row_idx])]
             for span in row_values:
-                if (span[0] in range(left_pos,right_pos) or 
-                    span[1]-1 in range(left_pos,right_pos)):
-                    touch_numbers.append(int(a[row_number+idx-1][span[0]:span[1]]))
+                if (span[0] in range(left_bound,right_bound) or 
+                    span[1]-1 in range(left_bound,right_bound)):
+                    touch_numbers.append(int(a[row_idx][slice(*span)]))
 
         if len(touch_numbers) == 2:
             sum_ratios = sum_ratios + (touch_numbers[0]*touch_numbers[1])
