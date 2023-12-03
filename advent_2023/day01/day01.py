a997af3b48a009139c504ccf220903403bc4c6ed
imadethis-smile <trentshapiro@gmail.com>
2023-12-03 16:29:35 -0600

    day01 python solution

---
+++ advent_2023/day01/day01.py
@@ -0,0 +1,27 @@
+import re
+
+with open("day01_input.txt") as f:
+    a = f.readlines()
+    a = [i.replace('\n','') for i in a]
+
+
+total = 0
+calibrated = 0
+for line in a:
+    nums = re.findall("[0-9]{1,1}", line)
+    first = int(nums[0])
+    last = int(nums[-1])
+
+    total = total + (first*10 + last)
+
+    for i,num in enumerate(["one","two","three","four","five","six","seven","eight","nine"]):
+        line = line.replace(num,num[0]+str(i+1)+num[-1])
+    
+    nums = re.findall("[0-9]{1,1}", line)
+    first = int(nums[0])
+    last = int(nums[-1])
+
+    calibrated = calibrated + (first*10 + last)
+
+print(total)
+print(calibrated)
