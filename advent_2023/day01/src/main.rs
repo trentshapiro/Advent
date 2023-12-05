cb00b13dabed1eccb74ce676b82c553efda06ba0
imadethis-smile <trentshapiro@gmail.com>
2023-12-04 21:09:19 -0600

    cleanup day1 rust

--- advent_2023/day01/src/main.rs
+++ advent_2023/day01/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use fancy_regex::Regex;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -8,106 +9,46 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-// fn first_digit(mixed_string: &str) -> i32 {
-//     for c in mixed_string.chars() {
-//         if c.is_numeric() {
-//             return c as i32 - 0x30
-//         }
-//     }
-//     return 0
-// }
+fn row_value(mixed_string: &str) -> i32{
+    let row_re = Regex::new(r"[0-9]{1,1}").unwrap();
+    let row_digits = row_re.find_iter(mixed_string)
+        .filter_map(|cap| cap.unwrap().as_str().parse().ok())
+        .collect::<Vec<i32>>();
 
-fn first_digit_with_words(mixed_str: &str) -> i32 {
-    let start_list = mixed_str.chars();
-    let mut build_str = String::from("");
+    let (a,b) = (row_digits.first().unwrap(), row_digits.last().unwrap());
 
-    for c in start_list {
-        // see if we have a number already
-        if c.is_numeric() {
-            return c as i32 - 0x30
-        };
-        // add current character to list, check for word
-        build_str.push(c);
-        //println!("{}", build_str);
-        let res = match build_str{
-            ref build_str if build_str.contains("one")   => 1,
-            ref build_str if build_str.contains("two")   => 2,
-            ref build_str if build_str.contains("three") => 3,
-            ref build_str if build_str.contains("four")  => 4,
-            ref build_str if build_str.contains("five")  => 5,
-            ref build_str if build_str.contains("six")   => 6,
-            ref build_str if build_str.contains("seven") => 7,
-            ref build_str if build_str.contains("eight") => 8,
-            ref build_str if build_str.contains("nine")  => 9,
-            _ => 0
-        };
-        if res > 0{
-            return res
-        };
-    }
-
-    return 0
+    return a*10+b
 
 }
 
-// fn last_digit(mixed_string: &str) -> i32 {
-//     for c in mixed_string.chars().rev() {
-//         if c.is_numeric() {
-//             return c as i32 - 0x30
-//         }
-//     }
-//     return 0
-// }
-
-fn last_digit_with_words(mixed_str: &str) -> i32 {
-    let start_list = mixed_str.chars().rev();
-    let mut build_str = String::from("");
-
-    for c in start_list {
-        // see if we have a number already
-        if c.is_numeric() {
-            return c as i32 - 0x30
-        };
-        // add current character to list, check for word
-        build_str.push(c);
-        //println!("{}", build_str);
-        let res = match build_str{
-            ref build_str if build_str.contains("eno")   => 1,
-            ref build_str if build_str.contains("owt")   => 2,
-            ref build_str if build_str.contains("eerht") => 3,
-            ref build_str if build_str.contains("ruof")  => 4,
-            ref build_str if build_str.contains("evif")  => 5,
-            ref build_str if build_str.contains("xis")   => 6,
-            ref build_str if build_str.contains("neves") => 7,
-            ref build_str if build_str.contains("thgie") => 8,
-            ref build_str if build_str.contains("enin")  => 9,
-            _ => 0
-        };
-        if res > 0{
-            return res
-        };
+fn sanitize_str(mixed_string: &str) -> String {
+    let mut string_out = mixed_string.to_string();
+    let num_words = vec!["one","two","three","four","five","six","seven","eight","nine"];
+    for (idx, num_word) in num_words.iter().enumerate(){
+        let fixed_str = num_word.to_string().chars().nth(0).unwrap().to_string() +
+                        &(idx+1).to_string() + 
+                        &num_word.to_string().chars().last().unwrap().to_string();
+        string_out = string_out.replace(num_word, &fixed_str)
     }
-
-    return 0
-
+    return string_out
 }
 
 fn main() {
-    let mut first;
-    let mut second;
     let mut running_total = 0;
+    let mut fixed_total = 0;
     // File must exist in the current path
     if let Ok(lines) = read_lines("./day01_input.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
-            if let Ok(ip) = line {
-                println!("{}", &ip);
-                first = first_digit_with_words(&ip);
-                second = last_digit_with_words(&ip);
-                running_total = running_total + first*10 + second;
-                println!("{} {} {}", first, second, running_total);
+            if let Ok(line_value) = line {
+                //println!("{}", &line_value);
+                running_total += row_value(&line_value);
+                fixed_total += row_value(&sanitize_str(&line_value));
             }
         }
     }
+    println!("{}", running_total);
+    println!("{}", fixed_total);
+
 }
 
