7d2864bf26021f192b6a2021caa1fccbfa7f1d96
imadethis-smile <trentshapiro@gmail.com>
2023-12-07 12:08:00 -0600

    day07 parts 1 and 2

--- advent_2023/day07/day07.py
+++ advent_2023/day07/day07.py
@@ -1,32 +1,43 @@
-import re
+from collections import Counter
 
-
-with open("day06_input.txt") as f:
+with open("day07_input.txt") as f:
     a = f.readlines()
     a = [i.replace('\n','') for i in a]
 
-ts = [int(i) for i in re.findall("\d+",a[0])]
-ds = [int(i) for i in re.findall("\d+",a[1])]
+#Part 1
+card_map = {"T":10,"J":11,"Q":12,"K":13,"A":14}
 
-wins = 1
-for i in range(0,len(ts)):
-    this_win_count = 0
-    for j in range(0,ts[i]):
-        dist = j*(ts[i]-j)
-        if dist > ds[i]:
-            this_win_count+=1
+final_scores = [[],[]]
+for line in a:
+    hand, bet = line.split(" ")
 
-    wins= wins * this_win_count
+    hand_1 = [int(i) if i.isnumeric() else card_map[i] for i in hand]
+    hand_2 = [i if i!=11 else 1 for i in hand_1]
+    hand_aggs = [Counter(hand_1), Counter(hand_2)]
 
-print(wins)
+    for idx, hand in enumerate([hand_1, hand_2]):
+        hand_counts = hand_aggs[idx]
 
-ts = int("".join([str(i) for i in ts]))
-ds = int("".join([str(i) for i in ds]))
+        non_joker = {k:v for k,v in hand_counts.items() if k !=1}
+        jokers = hand_counts[1] if 1 in hand_counts.keys()  else 0
 
-this_win_count = 0
-for j in range(0,ts):
-    dist = j*(ts-j)
-    if dist > ds:
-        this_win_count+=1
+        if len(non_joker.values()) == 0:
+            hand_value = 0
+        elif 3 in non_joker.values() and 2 in non_joker.values():
+            hand_value = 3.5
+        elif len([i for i in non_joker.values() if i == 2]) > 1:
+            hand_value = 2.5
+        else:
+            hand_value = max(non_joker.values())
+        
+        joker_value = hand_value + jokers
 
-print(this_win_count)
+        final_scores[idx].append(([joker_value,*hand], bet))
+
+
+for game in final_scores:
+    ordered_hands = sorted(game, key=lambda x: (x[0]))
+    total = 0
+    for idx,i in enumerate(ordered_hands):
+        total+= ((idx+1) * int(i[1]))
+    print(total)
