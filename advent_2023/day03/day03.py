7c2d222fc93a07d12c8c4dbc87650d1bdc206ab2
imadethis-smile <trentshapiro@gmail.com>
2023-12-07 14:53:18 -0600

    cleanup day3

--- advent_2023/day03/day03.py
+++ advent_2023/day03/day03.py
@@ -50,19 +50,18 @@ for row_number, row in enumerate(a):
         continue
 
     for left_pos, right_pos in star_positions:
-        touch_numbers = []
+        gear_nums = []
         left_bound = left_pos - 1
         right_bound = right_pos + 1
 
         for idx in range(0,3):
             row_idx = row_number+idx-1
             row_values = [i.span() for i in re.finditer("[0-9]{1,3}",a[row_idx])]
-            for span in row_values:
-                if (span[0] in range(left_bound,right_bound) or 
-                    span[1]-1 in range(left_bound,right_bound)):
-                    touch_numbers.append(int(a[row_idx][slice(*span)]))
+            for x,y in row_values:
+                if (x in range(left_bound,right_bound) or y-1 in range(left_bound,right_bound)):
+                    gear_nums.append(int(a[row_idx][x:y]))
 
-        if len(touch_numbers) == 2:
-            sum_ratios = sum_ratios + (touch_numbers[0]*touch_numbers[1])
+        if len(gear_nums) == 2:
+            sum_ratios = sum_ratios + (gear_nums[0]*gear_nums[1])
 
 print(sum_ratios)
