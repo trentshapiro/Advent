79bf419dc37d959bade80923b119f1173112861a
imadethis-smile <trentshapiro@gmail.com>
2023-12-07 15:05:17 -0600

    shorten day2

--- advent_2023/day02/day02.py
+++ advent_2023/day02/day02.py
@@ -4,18 +4,11 @@ with open("day02_input.txt") as f:
     a = f.readlines()
     a = [i.replace('\n','') for i in a]
 
-total = 0
-power = 0
-
+total, power = 0, 0
 for i, line in enumerate(a):
-    max_r = max([int(i) for i in re.findall("[0-9]{1,5}(?= red)",line)])
-    max_g = max([int(i) for i in re.findall("[0-9]{1,5}(?= green)",line)])
-    max_b = max([int(i) for i in re.findall("[0-9]{1,5}(?= blue)",line)])
-
-    if (max_r <= 12) and (max_g <= 13) and (max_b <= 14):
-        total += (i+1)
-
-    power = power + (max_r * max_b * max_g)
+    c = [max([int(i) for i in re.findall(f"[0-9]{{1,5}}(?= {i})",line)]) for i in ["red","green","blue"]]
+    total += (i+1) if (c[0] <= 12) and (c[1] <= 13) and (c[2] <= 14) else 0
+    power += (c[0]*c[1]*c[2])
 
 print(total)
 print(power)
