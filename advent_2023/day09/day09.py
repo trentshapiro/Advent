2751a9c1938480543e307bc91469bfd50900b613
imadethis-smile <trentshapiro@gmail.com>
2023-12-09 10:13:51 -0600

    day9 cleanup

--- advent_2023/day09/day09.py
+++ advent_2023/day09/day09.py
@@ -4,23 +4,23 @@ with open("day09_input.txt") as f:
     a = [i.replace('\n','') for i in a]
 
 
-extrapolation_right = 0
-extrapolation_left = 0
+sum_right = 0
+sum_left = 0
 for line in a:
     report = [[int(i) for i in line.split(" ")]]
 
     while not all([i==0 for i in report[-1]]):
         report.append([j-i for (i,j) in zip(report[-1],report[-1][1:])])
     
-    prediction_left, prediction_right = 0, 0
+    pred_left, pred_right = 0, 0
     for step in report[::-1]:
-        prediction_left = step[0] - prediction_left
-        prediction_right = prediction_right + step[-1]
+        pred_left = step[0] - pred_left
+        pred_right = pred_right + step[-1]
     
-    extrapolation_left+=prediction_left
-    extrapolation_right+=prediction_right
+    sum_left+=pred_left
+    sum_right+=pred_right
 
-print(extrapolation_right)
-print(extrapolation_left)
+print(sum_right)
+print(sum_left)
 
 
